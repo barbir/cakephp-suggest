083472137ee185a953dabf5a96899ad51e7edcec
Miljenko Barbir <miljenko.barbir@gmail.com>
2010-10-17 10:56:25 +0200

    * license changed from GNU GPL to MIT

--- suggest_app_model.php
+++ suggest_app_model.php
@@ -1,19 +1,12 @@
 <?php
 /*
-This file is part of CakePHP Suggest Plugin.
- 
-CakePHP Suggest Plugin is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
- 
-CakePHP Suggest Plugin is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
- 
-You should have received a copy of the GNU General Public License
-along with CakePHP Suggest Plugin. If not, see <http://www.gnu.org/licenses/>.
+ * This file is part of CakePHP Suggest Plugin.
+ *
+ * CakePHP Suggest Plugin
+ * Copyright (c) 2010, Miljenko Barbir (http://miljenkobarbir.com)
+ * 
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
 */
 
 class SuggestAppModel extends AppModel
--- views/suggest/index.ctp
+++ views/suggest/index.ctp
@@ -1,4 +1,14 @@
 <?php
+/*
+ * This file is part of CakePHP Suggest Plugin.
+ *
+ * CakePHP Suggest Plugin
+ * Copyright (c) 2010, Miljenko Barbir (http://miljenkobarbir.com)
+ * 
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+*/
+
     foreach ($data as $value)
 	{
 		echo "<li><a href='#' onclick=\"return false;\">$value</a></li>";
